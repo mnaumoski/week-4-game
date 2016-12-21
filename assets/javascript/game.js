c659ae344c8508875d97a371e815a3fd8a0e9e3d
mnaumoski <naumoski.marija@gmail.com>
2016-12-21 15:56:35 -0500

    applied constructive critcism

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,72 @@
 var beforeStart = alert("Welcome to Crystals Collector. Are you ready to play?");
 
-$( document ).ready(function() {
+$(document).ready(function() {
 
-	var numberToGuess = Math.floor(Math.random() * 100) + 19;//(max - min +1) + min
-	var counter = 0;
-	var colors = ["blue", "green", "red", "pink"];
-	var win = 0;
-	var loss = 0;
+    var numberToGuess = Math.floor(Math.random() * 100) + 19; //(max - min +1) + min
+    var counter = 0;
+    var colors = ["blue", "green", "red", "pink"];
+    var win = 0;
+    var loss = 0;
 
-	$("#number").text(numberToGuess);
+    $("#number").text(numberToGuess);
 
-	function createCrystals(){
-		$('#crystals').empty();
+    function createCrystals() {
+        $('#crystals').empty();
 
-		for (var i = 0; i < colors.length; i++) {
-			
-
-
-				var imageCrystal = $('<img>'); //variable that addresses <img> in html; this is different than .crystalImage that represents a class
-				
-				var clickCrystal = colors[i]+".jpeg";
-
-				// imageCrystal.attr('data-num', colors[i]);
-
-				imageCrystal.attr('src', clickCrystal);
-
-				imageCrystal.attr('alt', 'crystals');
-				
-				imageCrystal.addClass('crystalImage');
-
-				imageCrystal.attr("data-value", Math.floor(Math.random()*12) + 1);
-				
-				$('#crystals').append(imageCrystal);
-
-		}		
-	}
-	
-	createCrystals();
-
-	$(document).on("click", ".crystalImage", function(){
-			counter=counter+parseInt($(this).data('value'));// Need to understand this better; this.data('num')
-		
-			$("#yourNumber").text(counter);
-
-			if (counter == numberToGuess) {
-				alert('Congratulations! You won!');
-				win++;
-				$("#wins").text(win);
-				$("#yourNumber").empty(); 
-				counter = 0;
-				numberToGuess = Math.floor(Math.random()*100) + 19;//there has to be a better way than this...
-				$("#number").text(numberToGuess);
-				createCrystals();
-				
+        for (var i = 0; i < colors.length; i++) {
 
 
-			}
-			else if (counter > numberToGuess) {
-				alert('You lost!'); 
-				loss++;
-				$("#losses").text(loss);
-				$("#yourNumber").empty(); 
-				counter = 0;//this should be here; once I make the array a random number
-				numberToGuess = Math.floor(Math.random()*100) + 19;// this might not be DRY
-				$("#number").text(numberToGuess);
-				createCrystals();
-			
-		};
-	});
 
-	//  $("#restart")on("click", function () {
-	// 	location.reload();
-	// });
+            var imageCrystal = $('<img>'); //variable that addresses <img> in html; this is different than .crystalImage that represents a class
+
+            var clickCrystal = colors[i] + ".jpeg";
+
+            // imageCrystal.attr('data-num', colors[i]);
+
+            imageCrystal.attr('src', clickCrystal);
+
+            imageCrystal.attr('alt', 'crystals');
+
+            imageCrystal.addClass('crystalImage');
+
+            imageCrystal.attr("data-value", Math.floor(Math.random() * 12) + 1);
+
+            $('#crystals').append(imageCrystal);
+
+        }
+    }
+
+    createCrystals();
+
+    $(document).on("click", ".crystalImage", function() {
+        counter = counter + parseInt($(this).data('value')); // Need to understand this better; this.data('num')
+
+        $("#yourNumber").text(counter);
+
+        if (counter == numberToGuess) {
+            alert('Congratulations! You won!');
+            win++;
+            $("#wins").text(win);
+            $("#yourNumber").empty();
+            counter = 0;
+            numberToGuess = Math.floor(Math.random() * 100) + 19; //there has to be a better way than this...
+            $("#number").text(numberToGuess);
+            createCrystals();
 
 
-	});
 
-	 
+        } else if (counter > numberToGuess) {
+            alert('You lost!');
+            loss++;
+            $("#losses").text(loss);
+            $("#yourNumber").empty();
+            counter = 0; //this should be here; once I make the array a random number
+            numberToGuess = Math.floor(Math.random() * 100) + 19; // this might not be DRY
+            $("#number").text(numberToGuess);
+            createCrystals();
+
+        };
+    });
+
+
+});
